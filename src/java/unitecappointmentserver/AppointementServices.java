97bf5fb611d1b90eb258b9c9934c4b8fc1541d9e
nawaf77 <nawaf.s.h@hotmail.com>
2016-11-07 02:49:25 +1300

    Added Comments

--- src/java/unitecappointmentserver/AppointementServices.java
+++ src/java/unitecappointmentserver/AppointementServices.java
@@ -1,7 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * SOAP Web Services Class
+ *
+ * @author      Marzouq Almarzooq (1380949)
+ * @author      Nawaf Altuwayjiri (1377387)
  */
 package unitecappointmentserver;
 
